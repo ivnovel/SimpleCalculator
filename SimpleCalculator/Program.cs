639b9ff8c7316e1cf59cfa4dee578f9afa32cd92
ifeoluwa omotoso <ifeoluwaomotoso@gmail.com>
2021-01-08 14:12:50 +0100

    simple calculator

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -4,9 +4,64 @@ namespace SimpleCalculator
 {
     class Program
     {
+
+        
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            double firstNum;
+            double secondNum;
+            double result;
+            string action;
+
+            Console.WriteLine("\t\t\tThis is my Simple Calculator with CSharp\r");
+            Console.WriteLine("\t\t\t-----------------------------------------\r\n");
+
+            Console.WriteLine("\t\t\tEnter First Number \n");
+
+            firstNum = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\t\t\tSelect an Operator (+, -, /, *, %) \n");
+
+            action = Console.ReadLine();
+
+            Console.WriteLine("\t\t\tEnter Second Number \n");
+
+            secondNum = Double.Parse(Console.ReadLine());
+
+            if (action == "+")
+            {
+                result = firstNum + secondNum;
+                Console.WriteLine("\t\t\tThe answer is : " + result);
+            }
+            else if (action == "-")
+            {
+                result = firstNum - secondNum;
+                Console.WriteLine("\t\t\tThe answer is : " + result);
+            }
+            else if (action == "/")
+            {
+                result = firstNum / secondNum;
+                Console.WriteLine("\t\t\tThe answer is : " + result);
+            }
+            else if (action == "*")
+            {
+                result = firstNum * secondNum;
+                Console.WriteLine("\t\t\tThe answer is : " + result);
+            }
+            else if (action == "%")
+            {
+                result = firstNum % secondNum;
+                Console.WriteLine("\t\t\tThe answer is : " + result);
+            }
+            else
+            {
+                Console.WriteLine("Operator does not exist");
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
